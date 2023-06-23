329eae66ee4295ffe9a4b8a7fb0c3ffc7de358ff
Pascual Marcone <pascual.marcone@ug.uchile.cl>
2023-06-23 18:29:17 -0400

    Deleted random lines

--- src/main.cpp
+++ src/main.cpp
@@ -333,9 +333,6 @@ int getSpO2() {
 	}
 }
 
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
-
 ///////////////////////////
 //Funciones Sensor Fuerza//
 ///////////////////////////
